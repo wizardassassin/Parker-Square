83b0ccc61d4ef17bd76f949b9c91841173f66e6f
Victor Chen <60045243+wizardassassin@users.noreply.github.com>
2024-02-01 12:23:25 -0800

    Added output verification

--- run_program.sh
+++ run_program.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ "$1" = "main_mpi" ]; then
-    mpirun ./$1.out $2 $3 $4
+    mpirun --use-hwthread-cpus ./$1.out $2 $3 $4
 else
     ./$1.out --minValue=$2 --maxValue=$3 --vectorSize=$4 --threadCount=0
 fi

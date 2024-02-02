acf9dbde96394626bd22fdaf1e390896f9c7060b
Victor Chen <60045243+wizardassassin@users.noreply.github.com>
2024-02-01 19:03:22 -0800

    Fixed run_program logic

--- run_program.sh
+++ run_program.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ "$1" = "main_mpi" ]; then
+if [[ "$1" == *"mpi"* ]]; then
     mpirun --use-hwthread-cpus ./$1.out $2 $3 $4
 else
     ./$1.out --minValue=$2 --maxValue=$3 --vectorSize=$4 --threadCount=0

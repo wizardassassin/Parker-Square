64b8c53d8659ff5a315bfee9793c512a730d122b
Victor Chen <60045243+wizardassassin@users.noreply.github.com>
2024-05-10 15:14:03 -0700

    Added a file prefix option

--- threaded_compute/main_threadded.cpp
+++ threaded_compute/main_threadded.cpp
@@ -7,17 +7,18 @@
 #include "thread_pool.hpp"
 
 int main(int argc, char const* argv[]) {
-    if (argc != 3 && argc != 4) return 1;
+    if (argc < 3) return 1;
 
     const long long min_val = std::stoll(argv[1]);
     const long long max_val = std::stoll(argv[2]);
     const long long thread_count =
-        (argc == 4) ? std::stoll(argv[3]) : std::thread::hardware_concurrency();
+        (argc >= 4) ? std::stoll(argv[3]) : std::thread::hardware_concurrency();
+    const std::string prefix = (argc >= 5) ? argv[4] : "thread";
 
     ThreadPool::ThreadPool pool(thread_count);
     std::vector<std::ofstream> file_store(thread_count);
     for (long long i = 0; i < thread_count; i++)
-        file_store[i].open("thread_" + std::to_string(i) + "_" +
+        file_store[i].open(prefix + "_" + std::to_string(i) + "_" +
                            std::to_string(thread_count) + ".txt");
 
     std::vector<std::vector<std::vector<int64_t>>> vect_store(thread_count);
